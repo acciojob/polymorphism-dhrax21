a914cc074144e4f2f21a51658ebe9aeb963767ca
Dheeraj Singh <dhrax21bc@gmail.com>
2022-12-09 08:50:53 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,33 @@ package com.driver;
 
 public class Main {
 
+
+    static class Product{
+        public Product() {
+
+        }
+
+        public int product(int x ,int y)
+        {
+        }
+        public int product(int x,int y,int z)
+        {
+
+        }
+        public double product(double x,double y)
+        {
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Product p=new Product();
+
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(2.4,3.2);
+
+    }
+
 }
