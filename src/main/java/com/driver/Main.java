9b486c0290bb1928da5f27aa1b6ef6f288892129
Dheeraj Singh <112228200+dhrax21@users.noreply.github.com>
2022-12-10 12:58:13 +0530

    Second Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,14 +10,15 @@ public class Main {
 
         public int product(int x ,int y)
         {
+            return (x*y);
         }
         public int product(int x,int y,int z)
         {
-
+            return (x*y*z);
         }
         public double product(double x,double y)
         {
-
+                return (x*y);
         }
     }
 
@@ -25,9 +26,9 @@ public class Main {
 
         Product p=new Product();
 
-        p.product(1,2);
-        p.product(1,2,3);
-        p.product(2.4,3.2);
+        System.out.println(p.product(1,2));
+        System.out.println(p.product(1,2,3));
+        System.out.println(p.product(2.4,3.2));
 
     }
 
